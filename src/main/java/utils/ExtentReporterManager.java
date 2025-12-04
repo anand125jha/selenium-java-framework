0ab6e64aa2111a62fabca30afeb82306033a32d2
anand125jha <anand125jha@gmail.com>
2025-12-04 22:59:53 +0530

    removed reports folder

--- src/main/java/utils/ExtentReporterManager.java
+++ src/main/java/utils/ExtentReporterManager.java
@@ -3,7 +3,12 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 public class ExtentReporterManager {
     public static ExtentReports getReportObject(){
-        String path = System.getProperty("user.dir") + "/reports/AutomationReport.html";
+        String reportsDir = System.getProperty("user.dir") + "/reports";
+        java.io.File dir = new java.io.File(reportsDir);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        String path = reportsDir + "/AutomationReport.html";
         ExtentSparkReporter reporter = new ExtentSparkReporter(path);
         reporter.config().setReportName("Web Automation Results");
         reporter.config().setDocumentTitle("Test Results");
